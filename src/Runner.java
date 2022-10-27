d636619657291b216c2963abfe2c5eec475ff672
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-10-27 12:32:10 -0400

    Correctly updated what is being set into the variables

--- src/Runner.java
+++ src/Runner.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Provide a 3-digit integer: ");
+        System.out.print("Provide a 5-digit integer: ");
         int number = scanner.nextInt();
-        int numDigit1 = number / 100;
-        int numDigit2 = (number % 100) / 10;
-        int numDigit3 = number % 10;
-        System.out.println("" + numDigit3 + numDigit2 + numDigit1);
+        int numDigit1 = (number / 10000) + 1;
+        int numDigit2 = ((number % 10000) / 1000) + 1;
+        int numDigit3 = ((number % 1000) / 100) + 1;
+        int numDigit4 = ((number % 100) / 10) + 1;
+        int numDigit5 = (number % 10) + 1;
+        System.out.println("" + numDigit5 + "\n" + numDigit4 + "\n" + numDigit3 + "\n" + numDigit2 + "\n" + numDigit1);
     }
 }
